5431bd6d76ec6c1e753011e41788368f03a248c3
Jani Palsamäki <jani.palsamaki@gmail.com>
2020-02-04 12:50:49 +0200

    Retry login three times with 1 second delay

--- variables.py
+++ variables.py
@@ -1,6 +1,6 @@
 import os
 
 EXCEL_FILE_PATH = "./data/Data.xlsx"
-SWAG_LABS_URL = "https://www.saucedemo.com/"
+SWAG_LABS_URL = "https://www.saucedemo.com"
 SWAG_LABS_PASSWORD = os.getenv("SWAG_LABS_PASSWORD")
 SWAG_LABS_USER_NAME = os.getenv("SWAG_LABS_USER_NAME")
