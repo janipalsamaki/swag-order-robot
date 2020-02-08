08f64a846f51a6dab022f38657ba7a23c7dcf273
Jani Palsamäki <jani.palsamaki@gmail.com>
2020-02-08 10:35:47 +0200

    Add prepare.bat and run.bat for Windows

--- scripts/prepare.bat
+++ scripts/prepare.bat
@@ -1,14 +1,8 @@
-
-echo environment dump when starting
-set
-
 py -m venv venv
 call venv\Scripts\activate
 
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 webdrivermanager chrome
-
-python -m robot -d output swag-order-robot.robot
 
 call venv\Scripts\deactivate
